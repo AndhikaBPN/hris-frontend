c11c4361816afe01405ee0d1e679ef376cafe24f
AndhikaBPN <dikanusantara00@gmail.com>
2026-05-04 13:40:48 +0700

    feat: add unauthorized handle

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,23 @@
+function handleUnauthorized() {
+  localStorage.removeItem('hris_token');
+  localStorage.removeItem('hris_user');
+  var href = window.location.href;
+  var pagesMatch = href.match(/(.*\/pages\/)/);
+  window.location.replace(pagesMatch ? pagesMatch[1] + 'login.html' : 'login.html');
+}
+
+(function() {
+  var _originalFetch = window.fetch;
+  window.fetch = async function() {
+    var response = await _originalFetch.apply(this, arguments);
+    var url = typeof arguments[0] === 'string' ? arguments[0] : (arguments[0].url || '');
+    if (response.status === 401 && !url.endsWith('/login') && !url.endsWith('/config')) {
+      handleUnauthorized();
+    }
+    return response;
+  };
+})();
+
 async function apiRequest(path, options) {
   var requestOptions = options || {};
   var token = localStorage.getItem('hris_token');
@@ -7,9 +27,14 @@ async function apiRequest(path, options) {
     requestOptions.headers || {}
   );
 
-  var response = await fetch(await getApiUrl(path), Object.assign({}, requestOptions, {
+  var response = await fetch(getApiUrl(path), Object.assign({}, requestOptions, {
     headers: headers
   }));
+
+  if (response.status === 401 && path !== '/login') {
+    handleUnauthorized();
+    return;
+  }
 
   var data = await response.json().catch(function() {
     return {};
